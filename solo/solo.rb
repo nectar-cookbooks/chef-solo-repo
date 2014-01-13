e8fe1e1ecb5b5c2f3f3d017ceec39a4117bbf5aa
Stephen Crawley <s.crawley@uq.edu.au>
2014-01-13 13:12:29 +1000

    Fixes ...

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,5 +1,5 @@
 file_cache_path "/var/chef-solo"
-cookbook_path ["/var/chef-solo/cvl-chef/cookbooks",
-               "/var/chef-solo/cvl-chef/site-cookbooks"]
-role_path "/var/chef-solo/cvl-chef/roles"
-data_bag_path "/var/chef-solo/cvl-chef/data-bags"
+cookbook_path ["/var/chef-solo/chef-solo-repo/cookbooks",
+               "/var/chef-solo/chef-solo-repo/site-cookbooks"]
+role_path "/var/chef-solo/chef-solo-repo/roles"
+data_bag_path "/var/chef-solo/chef-solo-repo/data-bags"
