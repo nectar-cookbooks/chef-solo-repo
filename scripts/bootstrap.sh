f8992607790a81def03cb2de196d2df1730adf61
Stephen Crawley <s.crawley@uq.edu.au>
2014-01-30 16:28:50 +1000

    Various tweaks and fixes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-
 if [ -e /usr/bin/yum ] ; then
-  yum -y install curl git gcc gcc-c++ ruby-devel
+  yum -y -q install curl git gcc gcc-c++ ruby-devel
 else
-  apt-get -y install curl git build-essential ruby-dev
+  apt-get -y -qq update
+  apt-get -y -qq install curl git build-essential ruby-dev
 fi
 bash -c "curl -L https://www.opscode.com/chef/install.sh | bash"
 /opt/chef/embedded/bin/gem install --no-rdoc --no-ri berkshelf
 ln -s /opt/chef/embedded/bin/berks /usr/local/bin
 mkdir /var/chef-solo
 chown vagrant /var/chef-solo
-cd /var/chef-solo
-sudo -u vagrant git clone https://github.com/nectar-cookbooks/chef-solo-repo.git
-cd chef-solo-repo
-sudo -u vagrant cp solo/sample-node.json mynode.json
-sudo -u vagrant berks install --path cookbooks
-chef-solo -c solo/solo.rb -j mynode.json
+su - vagrant -c "cd /var/chef-solo ; git clone https://github.com/nectar-cookbooks/chef-solo-repo.git ; cd /var/chef-solo/chef-solo-repo ; cp solo/vagrant-node.json vagrant.json ; berks install --path cookbooks"
+chef-solo -c /var/chef-solo/chef-solo-repo/solo/solo.rb -j /var/chef-solo/chef-solo-repo/vagrant.json
