70ea7352183fbd911f1b736938938bf7348a2d4e
Stephen Crawley <s.crawley@uq.edu.au>
2014-04-14 10:49:01 +1000

    Added ssl_verify_mode ... per recommendation

--- solo/solo.rb
+++ solo/solo.rb
@@ -3,3 +3,7 @@ cookbook_path ["/var/chef-solo/chef-solo-repo/cookbooks",
                "/var/chef-solo/chef-solo-repo/site-cookbooks"]
 role_path "/var/chef-solo/chef-solo-repo/roles"
 data_bag_path "/var/chef-solo/chef-solo-repo/data-bags"
+
+# Recommend to protect against man-in-the-middle attacks
+ssl_verify_mode :verify_peer
+
