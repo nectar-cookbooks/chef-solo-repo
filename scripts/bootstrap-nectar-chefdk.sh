349b5313e074688d473528904cba23fff247e652
Stephen Crawley <s.crawley@uq.edu.au>
2014-11-24 11:01:49 +1000

    Tweaked so that the script can pick up overrides from the environment

--- scripts/bootstrap-nectar-chefdk.sh
+++ scripts/bootstrap-nectar-chefdk.sh
@@ -5,15 +5,15 @@
 echo "**** Installing required packages ****"
 if [ -e /usr/bin/yum ] ; then
   yum -y -q install curl git gcc gcc-c++ ruby-devel tar autoconf mysql-devel 
-  LOW_USER=ec2-user
+  ${LOW_USER:=ec2-user}
 else
   apt-get -y -qq update
   apt-get -y -qq install curl git build-essential libmysqlclient-dev
-  LOW_USER=ubuntu
+  ${LOW_USER:=ubuntu}
 fi
 
-TMP=/tmp
-VER=0.2.1-1
+${TMP:=/tmp}
+${VER:=0.2.1-1}
 
 echo
 echo "**** Installing ChefDK ****"
