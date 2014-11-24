f95efbfc5b20225df48ec1b08e7398682adb2ebb
Stephen Crawley <s.crawley@uq.edu.au>
2014-11-24 16:59:48 +1000

    Fix for previous + deal with the changed name convention in the ChefDK rpms.

--- scripts/bootstrap-nectar-chefdk.sh
+++ scripts/bootstrap-nectar-chefdk.sh
@@ -5,23 +5,27 @@
 echo "**** Installing required packages ****"
 if [ -e /usr/bin/yum ] ; then
   yum -y -q install curl git gcc gcc-c++ ruby-devel tar autoconf mysql-devel 
-  ${LOW_USER:=ec2-user}
+  echo operator is ${LOW_USER:=ec2-user}
 else
   apt-get -y -qq update
   apt-get -y -qq install curl git build-essential libmysqlclient-dev
-  ${LOW_USER:=ubuntu}
+  echo operator is ${LOW_USER:=ubuntu}
 fi
 
-${TMP:=/tmp}
-${VER:=0.2.1-1}
+echo temp directory is ${TMP:=/tmp}
+echo ChefDK version is ${VER:=0.2.1-1}
 
 echo
 echo "**** Installing ChefDK ****"
 if [ -e /usr/bin/yum ] ; then
-    # officially RHEL 6 only, but with luck this will work on recent Fedora 
+    # Officially RHEL 6 only, but with luck this will work on recent Fedora 
     # and RHEL 7 platforms too
-    curl https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-${VER}.el6.x86_64.rpm > $TMP/chefdk-${VER}.el6.x86_64.rpm
-    rpm -i $TMP/chefdk-${VER}.el6.x86_64.rpm
+    curl https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-${VER}.el6.x86_64.rpm > $TMP/chefdk-${VER}.x86_64.rpm
+    if [ "`file -b $TMP/chefdk-${VER}.x86_64.rpm`" = "XML document text" ] ; then
+	# They changed the RPM naming convention some time after 0.2.1-1
+	curl https://opscode-omnibus-packages.s3.amazonaws.com/el/6/x86_64/chefdk-${VER}.x86_64.rpm > $TMP/chefdk-${VER}.x86_64.rpm
+    fi
+    rpm -i $TMP/chefdk-${VER}.x86_64.rpm
 else
     # from ChefDK 0.2.0 onwards, there 12.04 deb is used on 13.10 also ...
     # and I guess later too
